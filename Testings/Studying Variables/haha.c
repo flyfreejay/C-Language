ca153b68776865f83aa5cf3814450d9bca2225b3
Jaehyung Lee <jaehyunglee@localhost.localdomain>
2019-05-16 21:05:16 -0400

    done for now

---
+++ Testings/Studying Variables/haha.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]){
+    int numberOfArgs = argc;
+    char *argument1 = argv[0];
+    char *argument2 = argv[1];
+
+    printf("%d\n", numberOfArgs);
+    printf("%s\n", argument1);
+    printf("%s\n", argument2);
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
