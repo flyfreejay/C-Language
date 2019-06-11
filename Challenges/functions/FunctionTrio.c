d969db83a076c300cf29d0307e550eff98d2d921
Jaehyung Lee <jaehyunglee@localhost.localdomain>
2019-06-10 21:58:08 -0400

    df

--- Challenges/functions/FunctionTrio.c
+++ Challenges/functions/FunctionTrio.c
@@ -5,12 +5,12 @@
     Purpose: To find the amount of weekly pay
 */
 #include <stdio.h>
-
-void gcd(int x, int y){
+#include <stlib.h>
+int Gcd(int x, int y){
     //hardcoding in arrays for divisors for both x and y
     int xdivisors[50] = {0};
     int ydivisors[50] = {0};
-
+    int gcd;
     int xIndex = 0;
     int yIndex = 0;
     int i, j;
@@ -19,6 +19,7 @@ void gcd(int x, int y){
     for (i = 1; i <= x/2; i++){
         
         if (x%i==0){
+            
             xdivisors[xIndex] = i;
             xIndex++;
             
@@ -44,10 +45,20 @@ void gcd(int x, int y){
         }
     }
 
-    
+    //compare commone values in both x-divisors and y-divisors
+    for (i = xIndex; i>=0; i--){
+        for (j = yIndex;j>=0; j--){
+            
+            if (xdivisors[i] == ydivisors[j]){
+                gcd = xdivisors[i];
+                break;
+            }
+        }
+    }
+    return gcd;
 }
 
 int main(){
-    gcd(4, 2);
+    printf("%d",Gcd(2,4));
     return 0;
 }
